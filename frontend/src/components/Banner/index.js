60e144e17fef0ee7cf254f0dd190379b4befd9bd
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-12-01 21:09:51 -0300

    Created logout button.

--- frontend/src/components/Banner/index.js
+++ frontend/src/components/Banner/index.js
@@ -1,18 +1,42 @@
 import React from "react";
-import Banner from "../../images/banner.png";
+import { useHistory, Link } from "react-router-dom";
+import { FiPower } from "react-icons/fi";
+
 import "../Banner/index.css";
-import { Link } from "react-router-dom";
+
+import Banner from "../../images/banner.png";
 
 function BannerBackground() {
+  const history = useHistory();
+
+  const studentId = localStorage.getItem("studentId");
+  const studentName = localStorage.getItem("studentName");
+
+  function handleLogout() {
+    localStorage.clear();
+
+    history.push("/");
+  }
+
   return (
     <div className="div-banner">
       <img alt="Matriculando" className="img-banner" fixed="top" src={Banner} />
-      <div className="title">
-        <Link className="font-link" to={"/"}>
-          {" "}
-          <h1 className="font-banner">Mλtriculando</h1>{" "}
-        </Link>
-      </div>
+      <header className="title">
+        {studentId && studentName ? (
+          <button
+            onClick={handleLogout}
+            className="logout-button"
+            type="button"
+          >
+            <FiPower size={18} color="#E02041" />
+          </button>
+        ) : (
+          <Link className="font-link" to={"/"}>
+            {" "}
+            <h1 className="font-banner">Mλtriculando</h1>{" "}
+          </Link>
+        )}
+      </header>
     </div>
   );
 }
