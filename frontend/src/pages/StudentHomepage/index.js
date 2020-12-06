b14521a2e4cd925034653356686439acc0e38a3d
MarceloLopes12 <marcelosouza323@gmail.com>
2020-12-06 16:02:30 -0300

    adicionado ultima integração de telas

--- frontend/src/pages/StudentHomepage/index.js
+++ frontend/src/pages/StudentHomepage/index.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import {
   Card,
   CardBody,
-  CardTitle,
-  CardSubtitle,
   Table,
   UncontrolledTooltip,
 } from "reactstrap";
@@ -31,66 +29,70 @@ function StudentHomepage() {
       });
   }, [studentId]);
 
+
   return (
     <>
       <BannerBackground />
 
-      <div
-        name="viewport"
-        content="width=device-width, initial-scale=1"
-        className="content-box"
-      >
-        <Card>
-          <CardBody className="">
-            <p>
-              Consulte o número minímo de alunos para esta turma{" "}
-              <span
-                style={{ textDecoration: "underline", color: "#333a56" }}
-                href="#"
-                id="UncontrolledTooltipExample"
+      <div className="container-box">
+        <div className="content-box" >
+          <Card>
+            <CardBody className="">
+              {/* <p>
+                Consulte o número minímo de alunos para esta turma{" "}
+                <span
+                  style={{ textDecoration: "underline", color: "#333a56" }}
+                  href="#"
+                  id="UncontrolledTooltipExample"
+                >
+                  AQUI
+                </span>
+                .
+              </p>
+              <UncontrolledTooltip
+                placement="top"
+                target="UncontrolledTooltipExample"
               >
-                AQUI
-              </span>
-              .
-            </p>
-            <UncontrolledTooltip
-              placement="top"
-              target="UncontrolledTooltipExample"
-            >
-              10 alunos.
-            </UncontrolledTooltip>
-            <br />
-            <Table className="table-list" dark>
-              <thead>
-                <tr>
-                  <th>Professor</th>
-                  <th>Disciplina</th>
-                  <th>Campus</th>
-                </tr>
-              </thead>
-              <tbody>
-                {/* <tr>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                </tr>
-                <tr>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-                </tr>
-                <tr>
-                  <td>Larry</td>
-                  <td>the Bird</td>
-                  <td>@twitter</td>
-                </tr> */}
-              </tbody>
-            </Table>
-            <br />
-            <br />
-            <br />
-          </CardBody>
-        </Card>
+                10 alunos.
+              </UncontrolledTooltip> */}
+              <br />
+              <Table className="table-list" dark>
+                <thead>
+                  <tr>
+                    <th>Professor</th>
+                    <th>Disciplina</th>
+                    <th>Campus</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    disciplines.map(discipline => 
+                      (<tr>
+                        <td>{discipline.discipline.professor[0].name || "Não definido"}</td>
+                        <td>
+                          <span
+                              href="#"
+                              id="UncontrolledTooltipExample"
+                            >
+                            {discipline.discipline.title}
+                            </span>
+                          <UncontrolledTooltip
+                            style={{ borderRadius: "2px", backgroundColor: "#ffffff", padding: "5px" }}
+                            placement="top"
+                            target="UncontrolledTooltipExample"
+                          >
+                            Número mínimo 30 alunos
+                          </UncontrolledTooltip>
+                        </td>
+                        <td>{discipline.discipline.campus.name}</td>
+                      </tr>)
+                    )
+                  }
+                </tbody>
+              </Table>
+            </CardBody>
+          </Card>
+        </div>
       </div>
     </>
   );
